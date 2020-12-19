e2467e1b9f0eb192b64e243c7485073eae8228aa
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2020-12-19 17:49:14 -0300

    first commit

---
+++ FN.Store/FN.Store.Api/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FN.Store.DI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FN.Store.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => {
+
+                // forçar a api a dar o erro 406
+                options.ReturnHttpNotAcceptable = true;
+
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            })
+                .AddJsonOptions(options => {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                });
+            services.AddDependencies();
+
+            services.AddSwaggerGen(s => {
+                s.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Store API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s => {
+                s.SwaggerEndpoint("swagger/v1/swagger.json", "Store API");
+                s.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
---
+++ FN.Store/FN.Store.Domain/Contracts/Repositories/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using FN.Store.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FN.Store.Domain.Contracts.Repositories
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> GetByNome(string name);
+
+        Task<IEnumerable<Produto>> GetAllWithCategoriaAsync();
+
+        Task<Produto> GetByIdWithCategoriaAsync(int id);
+    }
+}
